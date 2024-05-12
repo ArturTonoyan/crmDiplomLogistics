a99e1f62716cc74cfb602c82f0ea944f4262c92b
Artur <artur.tonoyan.04@list.ru>
2024-05-12 18:39:17 +0300

    v1

--- web/src/pages/AdminPages/HomePage/HomePage.jsx
+++ web/src/pages/AdminPages/HomePage/HomePage.jsx
@@ -1,50 +1,31 @@
 import React, { useEffect, useState } from "react";
-import styles from "./HomePage.module.scss"
-import { Link } from "react-router-dom";
-import Table from "./Table";
-import { tableHead, testData} from "./Data";
+import styles from "./HomePage.module.scss";
+import { tableHead, testData } from "./Data";
+import Table from "../../../components/Table/Table";
 function HomePage() {
-    const [tableHeader, settebleHeader] = useState([])
-    const [tableData, settableData] = useState([])
+  const [tableHeader, settebleHeader] = useState([]);
+  const [tableData, settableData] = useState([]);
 
-    useEffect(()=>{
-        settebleHeader(tableHead)
-        settableData(testData)
-    },[])
-    return (
-        <div className={styles.HomePage}>
-         
-        <div className={styles.HeadMenu}>
-         
-            <button>
-                Создать заказ
-            </button>
-            <button>
-                Редактировать заказ 
-            </button>
-            <button>
-                Удалить заказ
-            </button>
-            <button>
-                Добавить водителя
-            </button>
-            <button>
-                Добавить машину
-            </button>
-            <button>
-                Добавить клиента
-            </button>
-        </div>
+  useEffect(() => {
+    settebleHeader(tableHead);
+    settableData(testData);
+  }, []);
+  return (
+    <div className={styles.HomePage}>
+      <div className={styles.HeadMenu}>
+        <button>Создать заказ</button>
+        <button>Редактировать заказ</button>
+        <button>Удалить заказ</button>
+        <button>Добавить водителя</button>
+        <button>Добавить машину</button>
+        <button>Добавить клиента</button>
+      </div>
 
-          <div className={styles.Table}>
-            <Table
-                tableHeader={tableHeader}
-                tableData={tableData}
-            />
-          </div>
-
-        </div>
-      );
-    }
+      <div className={styles.Table}>
+        <Table tableHeader={tableHeader} tableData={tableData} />
+      </div>
+    </div>
+  );
+}
 
 export default HomePage;
